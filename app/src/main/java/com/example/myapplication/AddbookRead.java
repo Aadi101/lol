c4c5ecd2c92fd3f7f4676bd5b19804ef4ee2711d
Adithya <agrac98@gmail.com>
2019-08-03 18:15:55 +0530

    hmmm

--- app/src/main/java/com/example/myapplication/AddbookRead.java
+++ app/src/main/java/com/example/myapplication/AddbookRead.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddbookRead extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class AddbookRead extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addbook_read);
+    }
+    public void onAddDone(View view){
+        Intent intent1 = new Intent(AddbookRead.this,ReadingActivity.class);
+        startActivity(intent1);
     }
 }
